2f0ee150b61bab57c5dd0e8fe7bdf157c433d3d8
Brendan Woodward <73412688+bwoody13@users.noreply.github.com>
2023-11-27 15:07:53 -0500

    add notebook and python file to get optimal prices based on covariates (#2)
    Co-authored-by: bwoody13 <bwoody13@users.noreply.github.com>

---
+++ agents/ads-annihilators/optimizer.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pickle
+
+
+class Optimizer:
+    def __init__(self, model_file="xgbclassifier_v1.pkl"):
+        self.model = pickle.load(open(model_file, 'rb'))
+
+        # The following mins and maxes are from analysis of training data
+        # and found in the optimize revenue notebook (note these are slightly rounded)
+        self.min_p0 = 3.3
+        self.min_p1 = 0.01
+        self.max_p0_b = 90
+        self.max_p1_b = 133.45
+        self.max_p0 = 373
+        self.max_p1 = 548
+
+        # Make price combinations based on range of prices
+        self.price_combinations = self._make_prices_to_predict()
+
+    def _make_prices_to_predict(self):
+        '''
+        Make range of prices to check for optimality. We want there to be more options
+        in which there were items bought and consider fewer of the prices where there
+        was no purchase for that item.
+        :return: combination of all price pairs for item0 and item1
+        '''
+        p0_to_predict_comb = np.concatenate([
+            np.linspace(self.min_p0, self.max_p0_b, 150),
+            np.linspace(self.max_p0_b + 0.1, self.max_p0 / 3, 50),
+            np.linspace(self.max_p0 / 3 + 0.1, self.max_p0, 50)
+        ])
+        p1_to_predict_comb = np.concatenate([
+            np.linspace(self.min_p1, self.max_p1_b, 200),
+            np.linspace(self.max_p1_b + 0.1, self.max_p1 / 3, 75),
+            np.linspace(self.max_p0 / 3 + 0.1, self.max_p1, 75)
+        ])
+        p0_grid, p1_grid = np.meshgrid(p0_to_predict_comb, p1_to_predict_comb)
+        return np.column_stack([p0_grid.ravel(), p1_grid.ravel()])
+
+    def get_demand_at_prices(self, covs):
+        covs_repeated = np.repeat([covs], len(self.price_combinations), axis=0)
+        X = np.hstack([covs_repeated, self.price_combinations])
+        return self.model.predict_proba(X)[:, 0:-1]     # omit prob don't buy
+
+    def get_revenue_maximizing_prices_and_revenue(self, demand):
+        max_revenue = 0
+        max_prices = (0, 0)
+
+        for price_pair, demand in zip(self.price_combinations, demand):
+            p0, p1 = price_pair
+            demand0, demand1 = demand
+
+            revenue = p0 * demand0 + p1 * demand1
+
+            if revenue > max_revenue:
+                max_revenue = revenue
+                max_prices = (p0, p1)
+
+        return max_prices, max_revenue
+
+    def get_revenue_maximizing_prices_and_revenue_from_cov(self, covs):
+        return self.get_revenue_maximizing_prices_and_revenue(self.get_demand_at_prices(covs))
