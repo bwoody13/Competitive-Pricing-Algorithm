f68ff4826a3e0ed2239f876211d7aba4829e96cd
bwoody13 <bwoody13@users.noreply.github.com>
2023-12-01 17:03:58 -0500

    update ads_annihilators to ab agent

--- agents/ads_annihilators.py
+++ agents/ads_annihilators.py
@@ -1,10 +1,27 @@
+from agents.ads_annihilators.abs_agents.ab_test_agents import ABTestAgents
+from agents.ads_annihilators.abs_agents.ab_test_variant_attrs import ABTestVariantAttrs
 from agents.ads_annihilators.abs_agents.base_coop_exploit import BaseCoopExploit
 
 
-class Agent(BaseCoopExploit):
+class Agent(ABTestAgents):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args,
-                         moonshot=True,
-                         lower_alpha_threshold=0.2,
-                         lower_alpha_reset=0.4,
-                         **kwargs)
+        attributes = {
+            "exploit_threshold": 70,
+            "moonshot_threshold": 10
+        }
+        agent1 = BaseCoopExploit(*args,
+                                 moonshot=True,
+                                 lower_alpha_threshold=0.2,
+                                 lower_alpha_reset=0.4,
+                                 **kwargs)
+        ab1 = ABTestVariantAttrs(agent1, attributes, ab_length=250)
+
+        agent2 = BaseCoopExploit(*args,
+                                 single_alpha=True,
+                                 moonshot=True,
+                                 lower_alpha_threshold=0.2,
+                                 lower_alpha_reset=0.4,
+                                 **kwargs)
+        ab2 = ABTestVariantAttrs(agent2, attributes, ab_length=250)
+        super().__init__(ab1, ab2, ab_length=1000)
+
